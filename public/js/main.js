f7f1cc0ae7bb777579474c050a98721e9804649e
immanuelredd <immanuelokputu94@gmil.com>
2020-12-22 13:28:34 +0100

    modal update

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,9 @@ const harm = document.querySelector(".nav__harm");
 const body = document.querySelector("body");
 const nav = document.querySelector(".nav");
 const overly = document.querySelector(".overlay");
+const viewAll = document.getElementById("viewAll");
+const modalAll = document.querySelector(".modal");
+const close = document.querySelector(".close");
 let mob_nav = document.querySelector(".mobile_nav");
 let harmOpen = false;
 
@@ -16,6 +19,20 @@ harm.addEventListener("click", () => {
     overly.style.display = "none";
     body.classList.remove("noscroll");
     harmOpen = false;
+  }
+});
+
+viewAll.addEventListener("click", () => {
+  modalAll.style.display = "block";
+});
+
+close.addEventListener("click", () => {
+  modalAll.style.display = "none";
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modalAll) {
+    modalAll.style.display = "none";
   }
 });
 
